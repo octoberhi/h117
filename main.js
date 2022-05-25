aff7b5962a7dc848e3010a4945f9379e3c90f6a4
octoberhi <89272971+octoberhi@users.noreply.github.com>
2022-05-24 17:31:30 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+background("white");
+canvas.mouseReleased(classifyCanvas);
+synth = window.speechSynthesis;
+}
+
+function preload()
+{
+classifier = ml5.imageClassifier('DoodleNet');
+}
+
+
+function draw(){
+strokeWeight(13);
+stroke(0)
+if (mouseIsPressed){
+    line(pmouseX,pmouseY,mouseX,mouseY);
+}
+}
+
+function clear_canvas()
+{
+    background("white");
+}
+
+function classifyCanvas()
+{
+classifier.classify(canvas,gotResult);
+}
+
+
+function gotResult(error, results)
+{
+if (error){
+console.log(error);
+}
+console.log(results);
+document.getElementById("result_label").innerHTML = results[0].label;
+document.getElementById("result_Confidence").innerHTML = Math.round(results[0].confidence*100)+ "%";
+utterThis = new SpeechSynthesisUtterance(results[0].label);
+synth.speak(utterThis);
+}
